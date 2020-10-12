a2ee9cbbc03998364145b34e1d87c5b47f3f1bc7
Nikolai Klimkin <nd.klimkin@physics.msu.ru>
2020-10-12 13:04:36 +0300

    Article version

--- nn-cep.jl
+++ nn-cep.jl
@@ -1,26 +1,50 @@
-using Flux, CuArrays, JLD2, Random, Statistics, LinearAlgebra, FFTW
+using Flux, CUDA, JLD2, Random, Statistics, LinearAlgebra, FFTW, ProgressMeter
 
 using Flux: @epochs, train!, throttle
 
 using ProgressMeter
 
-const batch_size = 2048
+const batch_size = 512
+const aug_step = 22
 
+cubic = true
 chirp = true
 augment = true
-corr_pars = true
+ssh = false
+corr_pars = !ssh
 
-prefix = "2906_1917"#"2004_1549"#"
+learn_pars = true
+
+prefix = "0110_2037"#"0809_0958"##"3007_2230"#"2906_1917"#"2004_1549"#""0809_0958"#
 file = jldopen("resps-cep-$prefix.jld2")
-N_pars = size(file["data_freq"])[3]
-perm = randperm(N_pars)
+
+cep = permutedims(file["cep"])
+if chirp
+    try
+        global chp = permutedims(file["chirp"])
+    catch
+        global chirp = false
+    end
+end
+if cubic
+    try
+        global cbp = permutedims(file["cbp"])#).^(1//3)
+    catch
+        global cubic = false
+    end
+end
+
+exp_smp = (1:size(cep)[2])
+
 N_cycles = file["N"]
-pars = file["pars"]
-data = file["data_freq"]
+data = file["data_freq"][:,:,exp_smp]
+N_pars = size(data)[3]
+pars = ssh ? permutedims(hcat(file["delta"], file["h1"], file["h2"]))[:, exp_smp] : file["pars"][:,exp_smp]
+
+ord = size(pars)[1]
 
 if corr_pars
     κ = range(-π, stop=π, length=101)
-    ord = size(pars)[1]
 
     Threads.@threads for j=1:N_pars
         pars[1, j] += minimum([sum(pars[i,j]*cos((i-1)*κ) for i=2:ord) for κ=κ])
@@ -30,16 +54,7 @@ end
 Nω = size(data)[1]
 Nφ = size(data)[2]
 
-cep = permutedims(file["cep"])
-if chirp
-    try
-        global chp = permutedims(file["chirp"])
-    catch
-        chirp = false
-    end
-end
-
-N_test = 4
+N_test = 32
 
 N_train = N_pars - N_test*batch_size
 
@@ -47,25 +62,25 @@ data_tst = data[:,:,N_train+1:end]
 pars_tst = pars[:,N_train+1:end]
 cep_tst = cep[:,N_train+1:end]
 chirp && (chp_tst = chp[:,N_train+1:end])
+cubic && (cbp_tst = cbp[:,N_train+1:end])
 
 data_trn = data[:,:,1:N_train]
 pars_trn = pars[:,1:N_train]
 cep_trn = cep[:,1:N_train]
 chirp && (chp_trn = chp[:,1:N_train])
-
-#data_x_cv = Array{Float32}(undef, size(data)[1], size(data)[2], 3, size(data)[3])
+cubic && (cbp_trn = cbp[:,1:N_train])
 
 feed_fft = true
 
 if feed_fft
-    data_l = rfft(data, 2)
+    data_l = rfft(data_tst, 2)
 
-    imgs = reshape(abs.(data_l), :, N_pars)
-    imgs_phase_cos = reshape(real.(data_l), :, N_pars)
-    imgs_phase_sin = reshape(imag.(data_l), :, N_pars)
-    imgs_φ = reshape(data, :, N_pars)
+    imgs = reshape(abs.(data_l), :, N_pars-N_train)
+    imgs_phase_cos = reshape(real.(data_l), :, N_pars-N_train)
+    imgs_phase_sin = reshape(imag.(data_l), :, N_pars-N_train)
+    imgs_φ = reshape(data_tst, :, N_pars-N_train)
 
-    data_x_tst = vcat(imgs, imgs_phase_cos, imgs_phase_sin, imgs_φ)[:,N_train+1:end]
+    data_x_tst = vcat(imgs, imgs_phase_cos, imgs_φ)
 
     data_l_trn = rfft(data_trn, 2)
 
@@ -74,17 +89,18 @@ if feed_fft
     imgs_phase_sin = reshape(imag.(data_l_trn), :, N_train)
     imgs_φ = reshape(data_trn, :, N_train)
 
-    imgs_aug = copy(imgs)#Array{Float32}(undef, size(imgs)[1], N_pars*Nφ)
-    imgs_real_aug = copy(imgs_phase_cos)#Array{Float32}(undef, size(imgs)[1], N_pars*Nφ)
-    imgs_imag_aug = copy(imgs_phase_sin)#Array{Float32}(undef, size(imgs)[1], N_pars*Nφ)
-    imgs_φ_aug = copy(imgs_φ)#Array{Float32}(undef, size(imgs_φ)[1], N_pars*Nφ)
+    imgs_aug = copy(imgs)
+    imgs_real_aug = copy(imgs_phase_cos)
+    imgs_imag_aug = copy(imgs_phase_sin)
+    imgs_φ_aug = copy(imgs_φ)
 
     pars_aug = copy(pars_trn)
     cep_aug = copy(cep_trn)
     chirp && (chp_aug = copy(chp_trn))
+    cubic && (cbp_aug = copy(cbp_trn))
 
     if augment
-        @showprogress for (j, φ) = collect(enumerate(collect(range(0.0, stop=2π, length=Nφ+1))[1:end-1]))[1:8:end][2:end]
+        @showprogress "Preparing data..." for (j, φ) = collect(enumerate(collect(range(0.0, stop=2π, length=Nφ+1))[1:end-1]))[1:aug_step:end][2:end]
             data_shft = circshift(data_trn, (0, j-1, 0))
             data_l_shft = rfft(data_shft, 2)
 
@@ -96,48 +112,40 @@ if feed_fft
             global pars_aug = hcat(pars_aug, pars_trn)
             global cep_aug = hcat(cep_aug, cep_trn .- φ)
             chirp && (global chp_aug = hcat(chp_aug, chp_trn))
+            cubic && (global cbp_aug = hcat(cbp_aug, cbp_trn))
         end
     end
 
-    data_x = vcat(imgs_aug, imgs_real_aug, imgs_imag_aug, imgs_φ_aug)
+    data_x = vcat(imgs_aug, imgs_real_aug, imgs_φ_aug)
 else
     data_x = reshape(data, :, N_pars)
 end
 
-#=data_y = chirp ? vcat(pars_aug, chp_aug) : pars_aug
-data_yφ = vcat(cos.(cep_aug), sin.(cep_aug))
-data_yφ_tst = vcat(cos.(cep_tst), sin.(cep_tst))=#
-
 data_y = pars_aug
-data_yφ = chirp ? vcat(cos.(cep_aug), sin.(cep_aug), chp_aug) : vcat(cos.(cep_aug), sin.(cep_aug))
-data_yφ_tst = chirp ? vcat(cos.(cep_tst), sin.(cep_tst), chp_tst) : vcat(cos.(cep_tst), sin.(cep_tst))
+data_yφ = vcat(cos.(cep_aug), sin.(cep_aug), eval.([:chp_aug, :cbp_aug][BitArray([chirp, cubic])])...) #: vcat(cos.(cep_aug), sin.(cep_aug))
+data_yφ_tst = vcat(cos.(cep_tst), sin.(cep_tst), eval.([:chp_tst, :cbp_tst][BitArray([chirp, cubic])])...) #: vcat(cos.(cep_tst), sin.(cep_tst))
 
 ws = vcat(ones(ord), 0.01.*ones(2)) |> gpu
 
-display(size(data_x))
-display(size(data_y))
-display(size(data_yφ))
-
-#imgs .= imgs.*(1.0 .+ 0.01.*randn(size(imgs))) #log.(imgs) .* (1.0 .+ 2e-1.*randn(size(imgs)))
-
-#display(size(imgs))
-
 D1 = size(data_x)[1]
-D2 = 200
-D3 = 200
+D2 = 800
+D3 = 400
 D4 = ord+2
-D5 = 200
+D5 = 400
 D6 = size(data_y)[1]
 
 M_pars = Chain(BatchNorm(D1), Dense(D1, D2, swish), Dense(D2, D3, swish), Dense(D3, ord)) |> gpu
-M_cep = Chain(BatchNorm(D1), Dense(D1, D2, swish), Dense(D2, D3, swish), Dense(D3, chirp ? 3 : 2)) |> gpu
+M_cep = Chain(BatchNorm(D1), Dense(D1, D2, swish), Dense(D2, D3, swish), Dense(D3, D3, swish)) |> gpu
+
+phi = Chain(Dense(D3, 2, tanh)) |> gpu
+sphase = Dense(D3, chirp+cubic) |> gpu
 
 inds = collect(Iterators.partition(randperm(size(data_x)[2]), batch_size))
 N_batches = length(inds)
 
-P = gpu(data_x)
-Q = gpu(data_y)
-R = gpu(data_yφ)
+P = data_x |> gpu
+Q = data_y |> gpu
+R = data_yφ |> gpu
 
 train_loader_p = Flux.Data.DataLoader((P, Q), batchsize=batch_size, shuffle=true)
 train_loader_cep = Flux.Data.DataLoader((P, R), batchsize=batch_size, shuffle=true)
@@ -146,33 +154,55 @@ train_last = size(data_x)[2]-N_test*batch_size
 test_data_p = (gpu(data_x_tst), gpu(pars_tst))
 test_data_cep = (gpu(data_x_tst), gpu(data_yφ_tst))
 
-display(size.(test_data_p))
-display(M_pars)
-display(size(M_pars(test_data_p[1])))
-
-opt = ADAM(1f-3)
+opt = ADAM()
 
 evalcb() = (lng = length(test_data); test_loss = sum(L(test_data[i]...) for i=1:lng)/lng; @show(test_loss))
+diagonal(A::AbstractMatrix, k::Integer=0) = view(A, diagind(A, k))
 
-function train_NN!(N, model, loader, test_data)
-    @inline L(x, y) = Flux.mse(model(x), y)
-    for i=1:N
+function train_NN!(N, model, loader, test_data, L)
+    loss_best = L(test_data...)
+    pars_best = similar.(params(model))
+    [pars_best[i] .= copy(collect(params(model))[i]) for i=eachindex(pars_best)]
+
+    @showprogress "Training..." for i=1:N
         Flux.train!(L, params(model), loader, opt)
-        println("Epoch $i: ", L(test_data...))
+        loss_curr = L(test_data...)
+
+        if loss_best > loss_curr
+            loss_best = loss_curr
+            [pars_best[i] .= copy(collect(params(model))[i]) for i=eachindex(pars_best)]
+        end
     end
+
+    return pars_best
 end
 
-opt.eta=1f-3
-train_NN!(20, M_pars, train_loader_p, test_data_p)
-opt.eta = 1f-4
-train_NN!(20, M_pars, train_loader_p, test_data_p)
+L_pars(x, y) = Flux.mse(M_pars(x), y)
+
+if learn_pars
+    opt.eta=1f-3
+    train_NN!(40, M_pars, train_loader_p, test_data_p, L_pars)
+    opt.eta = 1f-4
+    train_NN!(20, M_pars, train_loader_p, test_data_p, L_pars)
+    opt.eta = 5f-5
+    band_model = train_NN!(10, M_pars, train_loader_p, test_data_p, L_pars)
+end
+
+CUDA.allowscalar(false)
+
+L_cep(x, y) = Flux.mse(phi(M_cep(x)), y[1:2, :]) + ((cubic || chirp) ? Flux.mse(sphase(M_cep(x)), y[3:end, :]) : 0f0)
 
 opt.eta = 1f-3
-train_NN!(20, M_cep, train_loader_cep, test_data_cep)
+train_NN!(40, (M_cep, phi, sphase), train_loader_cep, test_data_cep, L_cep)
 opt.eta = 1f-4
-train_NN!(20, M_cep, train_loader_cep, test_data_cep)
+train_NN!(20, (M_cep, phi, sphase), train_loader_cep, test_data_cep, L_cep)
+opt.eta = 5f-5
+pulse_model = train_NN!(10, (M_cep, phi, sphase), train_loader_cep, test_data_cep, L_cep)
 
 using BenchmarkTools
+
+Flux.loadparams!(M_pars, band_model)
+Flux.loadparams!((M_cep, phi, sphase), pulse_model)
 
 pars_eval = cpu(test_data_p[2])
 pars_infd = cpu(M_pars(test_data_p[1]))
@@ -180,26 +210,50 @@ pars_infd = cpu(M_pars(test_data_p[1]))
 Δ = pars_eval .- pars_infd
 Δ_rel = (pars_infd .- pars_eval)./pars_eval
 
-#display(collect(zip(pars_eval, pars_infd)))
-#println("")
+#display(collect(zip(pars_eval[:, 1:7], pars_infd[:, 1:7])))
+
+println("Absolute parameter errors:")
 println(round.(vec(std(Δ, dims=2)), digits=3))
 println("")
-println(round.(vec(sqrt.(mean(abs2.(Δ_rel), dims=2))), digits=3))
+println("Relative parameter errors:")
+println(round.(vec(sqrt.(mean(abs2.(Δ_rel), dims=2))), digits=3)) #if parameter changes sign, relative error will probably be greater than 1
 println("")
 
 cep_eval = test_data_cep[2][1:2, :] |> cpu
-cep_infd = M_cep(test_data_cep[1])[1:2, :] |> cpu
+cep_infd = phi(M_cep(test_data_cep[1])) |> cpu
 
 nrms = mapslices(norm, cep_infd, dims=1)
 cep_infd .= cep_infd./nrms
 
+#display(collect(zip(cep_eval[:, 1:7], cep_infd[:, 1:7])))
+
 cθ = diag(cep_eval'cep_infd)
 δφ = sqrt(2*mean(1.0 .- cθ))
+println("CEP error:")
 println(round(δφ, digits=3))
 
 if chirp
     chirp_eval = test_data_cep[2][3, :] |> cpu
-    chirp_infd = M_cep(test_data_cep[1])[3, :] |> cpu
-
+    chirp_infd = sphase(M_cep(test_data_cep[1]))[1, :] |> cpu
+    println("Chirp error:")
     println(round(std(vec(chirp_eval .- chirp_infd)), digits=3))
+end
+if cubic
+    cubic_eval = test_data_cep[2][4, :] |> cpu
+    cubic_infd = sphase(M_cep(test_data_cep[1]))[2, :] |> cpu
+    println("Cubic phase error:")
+    println(round(std(vec(cubic_eval .- cubic_infd)), digits=3))
+end
+
+pulse_true = cpu(test_data_cep[2])
+pulse_infd = vcat(cpu(phi(M_cep(test_data_cep[1]))), cpu(sphase(M_cep(test_data_cep[1]))))
+
+jldopen("results-$prefix.jld2", true, true, true, IOStream) do io
+    write(io, "data_tst", data_tst)
+    write(io, "pars_true", pars_eval)
+    write(io, "pars_infd", pars_infd)
+    write(io, "pulse_true", vcat(pulse_true, zeros(4-size(pulse_true)[1], size(pulse_true)[2])))
+    write(io, "pulse_infd", vcat(pulse_infd, zeros(4-size(pulse_true)[1], size(pulse_true)[2])))
+    write(io, "M1", cpu(collect(band_model)))
+    write(io, "M2", cpu(collect(pulse_model)))
 end
--- resp-2h-gpu.jl
+++ resp-2h-gpu.jl
@@ -1,27 +1,24 @@
 using LinearAlgebra, FFTW, Plots, Dates, ProgressMeter
 
-#using CuArrays, CUDAnative, CUDAdrv
 using CUDA
 using StaticArrays
-#using UnsafeArrays
-#using Suppressor
 
 println("$(Threads.nthreads()) threads")
 BLAS.set_num_threads(32)
 
 const mode = "cep"
+const ssh = false
 
 const f = 4.0
 const q = 0.1
 const ω = 0.8
 const T = 2π/ω
-const τ1 = Inf#2T
+const τ1 = Inf
 const τ2 = T/2
-const N_cycles = 10
-const Σ = T*N_cycles/10
-const μ = T*N_cycles/2
+const N_cycles = 40
+const Σ = T
+const t0 = T*N_cycles/2
 const Tmax = N_cycles*T
-#const φ = 0.0
 const z = 2
 
 const Nk = 32
@@ -34,63 +31,108 @@ const dt = Tmax/(2*hmax*N_cycles)
 const τs = range(0.0, stop=dt, length=Nph+1)[1:end-1] |> cu
 const dτ = dt/Nph
 const Φ = range(0.0, stop=2π, length=Nf+1)[1:end-1] |> cu
+const ξ = 2.0
 
 const β = 2.0
 smclamp(x, a, b) = (c = 0.5*(a+b); w = 0.5*(b-a); z=(x-c)/w; c + w*CUDA.tanh(CUDA.sinh(β*z)/β))
 const maxint = Tmax/2
 const fwhm = Tmax/2
 
-#EF(t) = CUDA.exp(-(t-μ)^2/(2Σ^2))
+cusqrt(x::ComplexF64) = CUDA.sqrt(CUDA.abs(x))*CUDA.exp(1.0im*CUDA.angle(x)*0.5)
+cusin(x::ComplexF64) = CUDA.imag(CUDA.exp(1im*x))
+cucos(x::ComplexF64) = CUDA.real(CUDA.exp(1im*x))
+cupow(x::ComplexF64, p::Float64) = CUDA.pow(CUDA.abs(x), p)*CUDA.exp(1.0im*p*CUDA.angle(x))
+
+function airy_asym(y::ComplexF64, n::Int, corr::ComplexF64 = zero(ComplexF64))
+    ret = 0.0
+    if CUDA.abs(CUDA.angle(y)) < 2π/3
+        z = y |> ComplexF64
+        ζ = (2/3)*cupow(z, 1.5)
+        cf = zero(ComplexF64)
+        u = one(Float64)
+        for k=0:n
+            cf += cupow(-1/ζ, Float64(k)) * u
+            u *= (6k+5)*(6k+3)*(6k+1)/((2k+1)*216*(k+1))
+        end
+        ret = cf*CUDA.exp(-ζ+corr)/2
+    else
+        z = -y |> ComplexF64
+        ζ = (2/3)*cupow(z, 3/2)
+        cf_c = zero(ComplexF64)
+        cf_s = zero(ComplexF64)
+        u = 1.0
+        for k=0:n
+            if k%2 == 0
+                cf_c += CUDA.pow(-1.0, k/2.0) / cupow(ζ, Float64(k)) * u
+            else
+                cf_s += CUDA.pow(-1.0, (k-1.0)/2.0) / cupow(ζ, Float64(k)) * u
+            end
+            u *= (6k+5)*(6k+3)*(6k+1)/((2k+1)*216*(k+1))
+        end
+        ret = CUDA.exp(corr)*(cf_c*cucos(ζ-π/4) + cf_s*cusin(ζ-π/4))
+    end
+    return ret/(sqrt(π)*cupow(z, 0.25))
+end
+
+function airy_taylor(z::ComplexF64, n::Int=40)
+    cf = 0
+    for k = 0:n
+        cf += CUDA.exp(CUDA.lgamma((k+1.0)/3) - CUDA.lgamma(k+1.0))*CUDA.sinpi(2*(k+1.0)/3)*cupow(CUDA.pow(3.0, 1/3)*z, Float64(k))
+    end
+    return (1.0/(CUDA.pow(3.0, 2/3)*π))*cf
+end
+
+function airy_gpu(z::ComplexF64, corr::ComplexF64, n1::Int64=20, n2::Int=1, r::Float64=2.5)
+    if CUDA.abs(z) < r
+        return airy_taylor(z, n1)*CUDA.exp(corr)
+    else
+        return airy_asym(z, n2, corr)
+    end
+end
+
+cusq3(x) = CUDA.sign(x)*CUDA.pow(CUDA.abs(x), 1.0/3)
 
 if mode == "2h"
     EF(t) = 1.0 - smclamp((t-(maxint+fwhm/4))/(fwhm/2), 0.0, 1.0) - smclamp(-(t-(maxint-fwhm/4))/(fwhm/2), 0.0, 1.0)
     A(φ::Float64, t::Float64) = EF(t)*f*(CUDA.sin(ω*t) + q*CUDA.sin(2ω*t+φ)/2)/ω
 elseif mode == "cep"
     const F0 = 0.0
-    EF(t) = CUDA.exp(-(t-μ)^2/(2Σ^2))
-    A(α::Float64, φ::Float64, t::Float64) = EF(t)*f*CUDA.sin(ω*t+φ+α*ω*(t-μ)^2/4Σ)/ω# + F0*t
+    cutf(t) = (t > ξ*T ? 1.0 : CUDA.exp(-(t-ξ*T)^2 /(2(ξ*T/4)^2)))
+    function EF(ϵ::Float64, β::Float64, z::Float64)
+        α = β*π*(Σ/2)^2
+        λ = ComplexF64(ϵ*π*Σ^3)
+        Σc = sqrt(Σ^2 + α^2/Σ^2)
+        μ = (Σ^2 - 1im*α)*(Σ/Σc)^2
+        exparg = cusq3(2/λ)*(z + μ^2 /2λ) |> ComplexF64
+        corr = (1/3)*μ^3/λ^2 +z*μ/λ
+        if CUDA.abs(corr) < 400
+            return 2*sqrt(π)*CUDA.abs(cusqrt(μ/2))*CUDA.pow(abs(2/λ), 1.0/3)*airy_gpu(exparg, corr, 25, 5, 3.5) #*exp(-z^2 /2μ)
+        else
+            return CUDA.exp(-1im*CUDA.angle(μ)/2)*CUDA.exp(-z^2/(2μ))
+        end
+    end
+    A(ϵ::Float64, β::Float64, φ::Float64, t::Float64) = cutf(t)*cutf(Tmax-t)*f*CUDA.imag(EF(ϵ, β, t-t0)*CUDA.exp(1im*(ω*t+φ)))/ω#+α*ω*(t-μ)^2/4Σ)/ω# + F0*t
 end
-@inline F(φ::Float64, t::Float64) = EF(t)*f*(cos(ω*t) + q*cos(2ω*t+φ))
 
 const σx = ComplexF64.([0 1; 1 0])
 const σy = ComplexF64.([0 -1im; 1im 0])
 const σz = ComplexF64.([-1 0; 0 1])
 const σ = [σx, σy, σz]
 
-function gen_U!(U, M, G, V, VC)
-    mul!(VC, G, V) #V3 == [0.0, 1.0]
-
-    @inbounds eϕ = VC[1]/CUDA.abs(VC[1])
-    @inbounds cθ = VC[2]/CUDA.sqrt(CUDA.abs2(VC[2]) + CUDA.abs2(VC[1]))
-
-    cθ2 = CUDA.sqrt((1+cθ)/2)
-    sθ2 = CUDA.sqrt((1-cθ)/2)
-
-    @inbounds U[1,1] = cθ2
-    @inbounds U[2,1] = -sθ2*eϕ
-    @inbounds U[1,2] = sθ2
-    @inbounds U[2,2] = cθ2*eϕ
-
-    return nothing
-end
-
 const ρ0 = (x = zeros(ComplexF64, z, z); x[1, 1] = 1.0; x) |> cu
 
 const idm = ComplexF64.(Matrix(I, z, z))
 const hid = ρ0
 
-#Nsp = 16
-const N_pars = 2^16
-#const N_pars = Nsp*Nsp
-const ord = 6
+const N_pars = 2^18
+const ord = 4
 const pars_bsl = [4.0, -1.0, 1.5, 1.0, 0.5, 0.5][1:ord]
 const pars_spr = [4.0, -5.0, -3.0, -2.0, -1.0, -1.0][1:ord]
 
 @assert length(pars_bsl) == length(pars_spr) == ord
 κ = range(-π, stop=π, length=101)
 
-const dips = (x = [0.0, 0.01]; vcat(x, zeros(ord-length(x)))) |> cu
-#const pars = (x = reduce(hcat, collect.(collect(Iterators.product(pars_bsl[1] .+ pars_spr[1].*range(0.0, stop=1.0, length=Nsp), pars_bsl[2] .+ pars_spr[2].*range(0.0, stop=1.0, length=Nsp))))); x[1,:] .-= x[2,:]; x)
+const dips = (x = [0.0, 0.05]; vcat(x, zeros(ord-length(x)))) |> cu
 pars_raw = pars_bsl .+ pars_spr.*rand(ord, N_pars)
 Threads.@threads for j=1:N_pars
     pars_raw[1, j] -= minimum([sum(pars_raw[i,j]*cos((i-1)*κ) for i=2:ord) for κ=κ])
@@ -98,17 +140,32 @@ end
 const pars = copy(pars_raw)
 if mode == "cep"
     const cep = 2π.*Base.rand(N_pars) |> cu
-    const chp = (-1.0 .+ 2.0 .* CUDA.rand(N_pars)).*0.5
+    const chp = 2.0 .*(-1.0 .+ 2.0 .* CUDA.rand(N_pars)) |> CuArray{Float64}#vcat(1.0 .+ 1.0 .* CUDA.rand(N_pars), -1.0 .- 1.0 .* CUDA.rand(N_pars))[1:2:end]
+    const cbp = 1.0 .*(-1.0 .+ 2.0 .* CUDA.rand(N_pars)) |> CuArray{Float64}#(-1.0 .+ 2.0 .* CUDA.rand(N_pars)).*0.5#2.0
 else
     const cep = CUDA.zeros(N_pars)
 end
 
 ht = zeros(ComplexF64, 2, 2, ord, N_pars)
 htr = zeros(ComplexF64, 2, 2, ord, N_pars)
-for i=1:N_pars
-    for j=1:ord
-        ht[:,:,j,i] = pars[j, i]*σz + (real(dips[j])*σx + imag(dips[j])*σy) /2
-        htr[:,:,j,i] = (-imag(dips[j])*σx + real(dips[j])*σy) /2 #switch to one-sided connections!
+
+if ssh
+    @assert ord == 2
+    δ = 0.0 .+ 0.0 .* rand(N_pars)
+    h1 = 3.0 .+ 0.0 .* rand(N_pars)
+    h2 = 4.0 .+ 0.0 .* rand(N_pars)
+
+    for i=1:N_pars
+        ht[:,:,1,i] = δ[i]*σz + h1[i]*σx
+        ht[:,:,2,i] = h2[i]*σx
+        htr[:,:,2,i] = h2[i]*σy
+    end
+else
+    for i=1:N_pars
+        for j=1:ord
+            ht[:,:,j,i] = pars[j, i]*σz + (real(dips[j])*σx + imag(dips[j])*σy) /2
+            htr[:,:,j,i] = (-imag(dips[j])*σx + real(dips[j])*σy) /2 #switch to one-sided connections!
+        end
     end
 end
 
@@ -117,74 +174,9 @@ const Htr = cu(htr)
 
 const freqs = (2π/Ts[end]).*collect(0:div(length(Ts), 2))
 
-function gpu_copy!(dest, src)
-    for a=1:2, b=1:2
-        @inbounds dest[a,b] = src[a,b]
-    end
-
-    return nothing
-end
-
-function gen_G!(G, M, ht_par, κ, ord)#, ::Val{ord}) where {ord}
-    lmul!(0f0, G)
-
-    for x=1:ord
-        for a=1:2, b=1:2
-            @inbounds M[a, b] = ht_par[a,b,x]
-        end
-        lmul!(CUDA.cos(κ*(x-1)), M)
-        for a=1:2, b=1:2
-            @inbounds G[a, b] += M[a, b]
-        end
-    end
-
-    return nothing
-end
-
-function gen_J!(J, M, ht_par, κ, ord)
-    lmul!(0f0, J)
-
-    for x=1:ord
-        for a=1:2, b=1:2
-            @inbounds M[a, b] = ht_par[a,b,x]
-        end
-        lmul!(-CUDA.sin(κ*(x-1))*(x-1), M)
-        for a=1:2, b=1:2
-            @inbounds J[a, b] += M[a, b]
-        end
-    end
-    return nothing
-end
-
-function write_tr!(dest, src, inds)
-    for a=1:2
-        @inbounds dest[inds...] += real(src[a, a])
-    end
-    return nothing
-end
-
-function pow!(M, R, J, ν)
-    for a=1:2, b=1:2
-        @inbounds J[a, b] = R[a, b]
-    end
-
-    @inbounds mul!(M, R, J)
-    for a=1:2, b=1:2
-        @inbounds J[a, b] = M[a, b]
-    end
-
-    for a=1:2, b=1:2
-        @inbounds R[a, b] = J[a, b]
-    end
-
-    return nothing
-end
-
 const ν = 4
 
-function statsimkern!(chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, exc) where {Z} where {ORD}
-    #i = threadIdx().x
-    #j = threadIdx().y
+function statsimkern!(cbp, chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, exc) where {Z} where {ORD}
     k_id = threadIdx().x
     k_stride = blockDim().x
     φ_id = threadIdx().y
@@ -213,11 +205,11 @@ function statsimkern!(chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, e
     V[2] = 1.0
 
     if exc
-        (k_id == 1 && φ_id == 1 && block_id == 1) && @cuprintf("Memory allocated\n")
-        for i=k_id:k_stride:Nk, j=φ_id:φ_stride:Nf, q=block_id:block_stride:N_pars
+        for i=k_id:k_stride:Nk, j=φ_id:φ_stride:Nf, q=block_id:block_stride:chunk_size
             @inbounds k = K[i]
             @inbounds φ = cep[q] + Φ[j]
             @inbounds α = chp[q]
+            @inbounds λ = cbp[q]
 
             for a=1:Z, b=1:Z, x=1:ORD
                 @inbounds HT[a, b, x] = hts[a,b,x,q]
@@ -241,7 +233,7 @@ function statsimkern!(chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, e
             cθ2 = CUDA.sqrt((1f0+cθ)*0.5f0)
             sθ2 = CUDA.sqrt((1f0-cθ)*0.5f0)
 
-            @inbounds U[1,1] = -cθ2#-cθ2
+            @inbounds U[1,1] = -cθ2
             @inbounds U[1,2] = sθ2
             @inbounds U[2,1] = sθ2*eϕ
             @inbounds U[2,2] = cθ2*eϕ
@@ -254,11 +246,13 @@ function statsimkern!(chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, e
 
                 fill!(J, 0f0)
 
+                fld = A(λ, α, φ, t)
+
                 for x=1:ORD, a=1:Z, b=1:Z
-                    @inbounds J[a, b] += -HT[a,b,x]*CUDA.sin((k + A(α, φ, t))*(x-1))*(x-1)
+                    @inbounds J[a, b] += -HT[a,b,x]*CUDA.sin((k + fld)*(x-1))*(x-1)
                 end
                 for x=1:ORD, a=1:Z, b=1:Z
-                    @inbounds J[a, b] += HTR[a,b,x]*CUDA.cos((k + A(α, φ, t))*(x-1))*(x-1)
+                    @inbounds J[a, b] += HTR[a,b,x]*CUDA.cos((k + fld)*(x-1))*(x-1)
                 end
 
                 mul!(M, J, ρ)
@@ -268,18 +262,20 @@ function statsimkern!(chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, e
                 for τ = τs
                     fill!(G, 0f0)
 
+                    fld = A(λ, α, φ, t+τ)
+
                     for x=1:ORD, a=1:Z, b=1:Z
-                        @inbounds G[a, b] += dτ*HT[a,b,x]*CUDA.cos((k + A(α, φ, t+τ))*(x-1))/ν
+                        @inbounds G[a, b] += dτ*HT[a,b,x]*CUDA.cos((k + fld)*(x-1))/ν
                     end
                     for x=1:ORD, a=1:Z, b=1:Z
-                        @inbounds G[a, b] += dτ*HTR[a,b,x]*CUDA.sin((k + A(α, φ, t+τ))*(x-1))/ν
+                        @inbounds G[a, b] += dτ*HTR[a,b,x]*CUDA.sin((k + fld)*(x-1))/ν
                     end
 
                     @inbounds δ = CUDA.abs2(G[2,2])
                     @inbounds ϵ = CUDA.abs2(G[1,2])
                     σ = CUDA.sqrt(δ+ϵ)
 
-                    R .= ID.*CUDA.cos(σ) .+ G .* (1im*CUDA.sin(σ)/σ)
+                    R .= ID.*CUDA.cos(σ) .+ G .* (1im*CUDA.sin(σ)/σ) #exponentiation procedure. only yields correct results for traceless G
 
                     eϕ = @inbounds G[2, 1]/CUDA.abs(G[1, 2])
                     cθ = @inbounds G[2, 2]/CUDA.sqrt(CUDA.abs2(G[1, 2]) + CUDA.abs2(G[2, 2])) |> real
@@ -300,7 +296,7 @@ function statsimkern!(chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, e
                         mul!(M, J, U)
 
                         for a=1:2
-                            @inbounds M[a, 3-a] = 0f0 #fuck this line of code in particular
+                            @inbounds M[a, 3-a] = 0f0
                         end
 
                         mul!(J, U, M)
@@ -318,72 +314,61 @@ function statsimkern!(chp, cep, js, hts, htr, ::Val{Z}, ::Val{ORD}, ::Val{ν}, e
     return nothing
 end
 
-out = zeros(ComplexF64, length(freqs), Nf, N_pars)
-out_J = CUDA.zeros(Float64, length(Ts), Nf, N_pars)
+const chunk_size = 2048
+
+out_Jωc = CUDA.zeros(ComplexF64, 1+hmax*N_cycles, Nf, chunk_size)
+out_Jω = zeros(1+hmax, Nf, N_pars)
+
+const inds = Iterators.partition(1:N_pars, chunk_size)
+
+out_J = CUDA.zeros(Float64, length(Ts), Nf, chunk_size)
+
+out_Js = zeros(Float64, length(Ts), Nf)
 
 @time begin
-    @cuda threads=(1, 1) blocks=1 statsimkern!(chp, cep, out_J, Ht, Htr, Val(2), Val(ord), Val(ν), false);
+    @cuda threads=(1, 1) blocks=1 statsimkern!(cbp, chp, cep, out_J, Ht, Htr, Val(2), Val(ord), Val(ν), false);
     Array(out_J)
 end
 
-out_J .= 0.0
-
 df = Dates.format(now(), "ddmm_HHMM")
 
-@time begin
-    @sync @cuda threads=(8, 8) blocks=512 statsimkern!(chp, cep, out_J, Ht, Htr, Val(2), Val(ord), Val(ν), true);
-    Array(Ht)
+rfft_plan = plan_rfft(out_J, 1)
+
+@showprogress for (j, ind)=enumerate(inds)
+    fill!(out_J, 0.0)
+    @sync @cuda threads=(8, 8) blocks=512 statsimkern!(cbp[ind], chp[ind], cep[ind], out_J, Ht[:,:,:,ind], Htr[:,:,:,ind], Val(2), Val(ord), Val(ν), true);
+    (j == 1) && (out_Js .= Array(@view(out_J[:,:,1])))
+    mul!(out_Jωc, rfft_plan, out_J)
+    @view(out_Jω[:,:,ind]) .= abs.(Array(@view(out_Jωc[1:N_cycles:end, :, :])))
 end
-
-println("Simulation complete")# abs.(rfft(cmp, 1)) |> Array
-
-const chunk_size = Int(round(sqrt(N_pars)))
-const inds = Iterators.partition(1:N_pars, chunk_size)
-
-out_Jω = zeros(1+hmax*N_cycles, Nf, N_pars)
-proc_J = CUDA.zeros(length(Ts), Nf, chunk_size)
-
-if mode == "2h"
-    @showprogress for ind=inds
-        @view(out_Jω[:,:,ind]) .= abs.(Array(rfft(@view(out_J[:,:,ind]), 1)))# |> Array
-    end
-elseif mode == "cep"
-    @showprogress for ind=inds
-        @view(out_Jω[:,:,ind]) .= abs.(Array(rfft(@view(out_J[:,:,ind]), 1)))# |> Array
-    end
-
-    out_Jωl = rfft(out_Jω, 2)
-end
-
-println("RFFT done")
+println("Simulation complete")
 
 display(size(out_Jω))
-
-#using BSON
-#using BSON: @save, @load
-
-#@save "resps-$df.bson" out_Jω pars
 
 using JLD2
 
 prs = Float32.(copy(pars)) |> Array
 
 jldopen("resps-$mode-$df.jld2", true, true, true, IOStream) do io
-    #write(io, "data", Array(Float64.(out_J)))
     if mode == "2h"
-        write(io, "data_freq", Float64.(real.(out_Jω))[1:N_cycles:end, :, :])
+        write(io, "data_freq", Float64.(abs.(out_Jω)))
     elseif mode == "cep"
-        write(io, "data_freq", Float64.(abs.(out_Jω))[1:N_cycles:end, :, :])
-        #write(io, "data_angfreq", Float64.(abs.(out_Jωl))[1:N_cycles:end, :, :]) #WARNING: ROTATE BEFORE USE
-        #write(io, "data_angfreq_cos", Float64.(real.(out_Jωl))[1:N_cycles:end, :, :])
-        #write(io, "data_angfreq_sin", Float64.(imag.(out_Jωl))[1:N_cycles:end, :, :])
+        write(io, "data_freq", Float64.(abs.(out_Jω)))
         write(io, "cep", Array(cep))
+        write(io, "cbp", Array(cbp))
+        write(io, "chirp", Array(chp))
+        write(io, "out_Js", out_Js)
     end
     write(io, "freqs", freqs)
     write(io, "omega0", ω)
     write(io, "T", Ts)
     write(io, "phi", Φ)
     write(io, "N", N_cycles)
-    write(io, "pars", prs)
-    write(io, "chirp", Array(chp))
+    if ssh
+        write(io, "delta", δ)
+        write(io, "h1", h1)
+        write(io, "h2", h2)
+    else
+        write(io, "pars", prs)
+    end
 end
